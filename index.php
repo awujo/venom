436810b9806449fafdc41d48cbd8c9f66926ccc5
awujo <71509142+awujo@users.noreply.github.com>
2022-04-10 15:46:03 -0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="w3hubs.com">
+	  <meta name="author" content="w3hubs.com">
+	   <link rel = "icon" href = 
+"https://i.ibb.co/HFwYZW1/PNG-images-Snake-28png.png" 
+        type = "image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300i,400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
